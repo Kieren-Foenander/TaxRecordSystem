a61a0ecdc225127195a2c7dcc03292e533884385
Kieren Foenander <kieren.foenander@cqumail.com>
2021-04-13 20:28:43 +1000

    first commit

---
+++ src/taxrecordsystem/TaxRecordSystem.java
@@ -0,0 +1,21 @@
+/*
+Programmer: Kieren Foenander S012141776
+Course: COIT12200 Software Design and Development
+Assignment 2
+ */
+package taxrecordsystem;
+
+/**
+ *
+ * @author black
+ */
+public class TaxRecordSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
