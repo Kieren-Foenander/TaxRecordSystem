be75f34232d13726eb8dbc2d417c7f31efc8cf01
Kieren Foenander <kieren.foenander@cqumail.com>
2021-05-04 14:27:32 +1000

    customer class completed

--- src/taxrecordsystem/TaxRecordSystem.java
+++ src/taxrecordsystem/TaxRecordSystem.java
@@ -15,7 +15,9 @@ public class TaxRecordSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
+       
     }
-    
+
 }
